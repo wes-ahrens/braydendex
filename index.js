ffed2bd82188f6a2bda2909ad61f5daf0038b1c8
Wes Ahrens <wes.ahrens@gmail.com>
2018-11-24 00:31:10 -0500

    Fix formatting

--- index.js
+++ index.js
@@ -1,69 +1,53 @@
-'use strict';
+'use strict'
 
 // Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
-const {WebhookClient} = require('dialogflow-fulfillment');
+// const { dialogflow } = require('actions-on-google')
+const { WebhookClient } = require('dialogflow-fulfillment')
 
 const PORT = process.env.PORT || 8080
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 function pokedexNumber (agent) {
-	console.log(agent.parameters);
-	const number = agent.parameters.number;
-	const request = require('request-promise-native');
-	const url = 'https://pokeapi.co/api/v2/pokemon/'+number;
-	console.log(url);
-	return request.get(url)
-		.then( jsonBody => {
-			var body = JSON.parse(jsonBody);
-			agent.add('Pokemon with pokedex number ' + number + ' is ' + body.name);
-			return Promise.resolve(agent);
-		})
-//	agent.add('Pokemon with pokedex number 1 is bulbasaur');
+  console.log(agent.parameters)
+  const number = agent.parameters.number
+  const request = require('request-promise-native')
+  const url = 'https://pokeapi.co/api/v2/pokemon/' + number
+  console.log(url)
+  return request.get(url)
+    .then(jsonBody => {
+      var body = JSON.parse(jsonBody)
+      agent.add('Pokemon with pokedex number ' + number + ' is ' + body.name)
+      return Promise.resolve(agent)
+    })
 }
 
-function WebhookProcessing(req, res) {
-	const agent = new WebhookClient({request: req, response: res});
-	console.info('agent set');
+function pokedexNumberColour (agent) {
+  console.log(agent.parameters)
+  console.log('asking for colour')
+  agent.add('Not sure what colour the pokemon is')
+}
 
-	let intentMap = new Map();
-	intentMap.set('pokedex number', pokedexNumber);
-	agent.handleRequest(intentMap);
+function WebhookProcessing (req, res) {
+  const agent = new WebhookClient({ request: req, response: res })
+  console.info('agent set')
+
+  let intentMap = new Map()
+  intentMap.set('pokedex number', pokedexNumber)
+  intentMap.set('pokedex number_colour', pokedexNumberColour)
+  agent.handleRequest(intentMap)
 }
 
 // Webhook
 app.post('/', function (req, res) {
-	console.info('\n\n>>>>>> S E R V E R    H I T <<<<<<<');
-	WebhookProcessing(req, res);
-});
+  console.info('POST ' + req)
+  WebhookProcessing(req, res)
+})
 
-app.listen(PORT, function() {
-	console.info('Webhook listening on port ' + PORT);
-});
-
-// Instantiate the Dialogflow client.
-//const app = dialogflow({debug: true});
-
-// Handle the Dialogflow intent named 'pokedex number'.
-// The intent collects a parameter named 'number'.
-//app.intent('pokedex number', (conv, {number}) => {
-//    var rp = require('request-promise-native');
-
-//    var options = {
-//      uri: 'https://pokeapi.co/api/v2/pokemon/'+number,
-//      json: true
-//    }
-//    return rp(options)
-//      .then( response => {
-//        console.log( 'response:', JSON.stringify(response, null, 1) );
-//	var value = response.name;
-//	return conv.close('Pokemon with pokedex number ' + number + ' is ' + value);
-//    });
-//
-//});
-
+app.listen(PORT, function () {
+  console.info('Webhook listening on port ' + PORT)
+})
