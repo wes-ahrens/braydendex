5484d7a4bad1d35f6a7ae047f19aee4f429e5f02
Wes Ahrens <wes.ahrens@gmail.com>
2019-10-04 22:39:29 -0400

    Use materialize/nunjucks for webpage

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const PORT = process.env.PORT || 8080
 const express = require('express')
+const nunjucks = require('nunjucks')
 const bodyParser = require('body-parser')
 const { dialogflow } = require('./dialogflow/dispatch')
 
@@ -10,9 +11,23 @@ server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(express.static('public'))
 
+const templatePath = './templates'
+nunjucks.configure(templatePath, {
+  autoescape: true,
+  express: server
+})
+
 // Webhook
 server.post('/api', dialogflow)
 server.post('/dialogflow/api', dialogflow)
+
+server.get('/index.html', (req, res) => {
+  return res.render('index.html')
+})
+
+server.get('/about.html', (req, res) => {
+  return res.render('about.html')
+})
 
 server.get('/status', function (req, res) {
   console.info('GET status request received')
---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav')
+  M.Sidenav.init(elems, {
+    draggable: true
+  })
+})
